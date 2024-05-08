e1d8ed54193fc805799d15168fe2d2f247662d32
Cristinacbm5 <157827345+Cristinacbm5@users.noreply.github.com>
2024-05-08 06:18:15 +0000

    First Commit

--- src/app.js
+++ src/app.js
@@ -2,10 +2,31 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  const palos = ["♠", "♣", "♥", "♦"];
+  const paloIndex = Math.floor(Math.random() * 4);
+  const numeroCarta = Math.floor(Math.random() * 13) + 1;
+  const cartas = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  const carta = document.getElementById("carta");
+  console.log(carta);
+  carta.innerHTML = cartas[numeroCarta];
+  const palo = document.querySelectorAll("#palos");
+  for (let index = 0; index < palo.length; index++) {
+    palo[index].classList.add(palos[paloIndex]);
+    palo[index].innerHTML = palos[paloIndex];
+  }
 };
